d1de1e537047f47bd6366f7fbbd51c1622102dcd
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-06-27 08:02:07 -0700

    setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div>
+      <p className="text-5xl text-primary-400 font-rublik">
+        quickBrown FOX jumps over lazy dog
+      </p>
+      <p className="text-5xl text-primary-400 font-lato">
+        quickBrown FOX jumps over lazy dog
+      </p>
+      <p className="text-5xl text-primary-400 font-oxygen">
+        quickBrown FOX jumps over lazy dog
+      </p>
+      <p className="text-5xl text-primary-400 font-poppins">
+        quickBrown FOX jumps over lazy dog
+      </p>
+    </div>
+  );
+}
+
+export default App;
